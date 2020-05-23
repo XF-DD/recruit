0c6c019ad656eca498c796a0b196d4eb3f33a1df
EdwardGit <pocketknifegithub@163.com>
2020-05-23 15:46:51 +0800

    面试信息返回ischeck,msgSendTime \ hr关联公司 \ 搜索返回状态

--- src/main/java/qdu/java/recruit/mapper/InterviewMapper.java
+++ src/main/java/qdu/java/recruit/mapper/InterviewMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface InterviewMapper {
 
     //查询所有轮次或者更多的所有面试信息（不同面试轮数，对应职位）。
-    @Select("select m.news,a.state,m.msgSendTime,a.applicationId,u.*,p.title \n"+
+    @Select("select m.news,a.state,m.msgSendTime,m.isCheck,a.applicationId,u.*,p.title \n"+
             "from user u join application a on u.userId = a.userId \n"+
             "join position  p on p.positionId = a.positionId \n"+
             "join message m on a.applicationId = m.applicationId \n"+
@@ -24,7 +24,7 @@ public interface InterviewMapper {
     //查询所有轮次或者更多的对应职位的所有面试信息（不同面试轮数，对应职位）。
     @Select({
             "<script>",
-            "select m.news,a.state,m.msgSendTime,a.applicationId,u.*,p.title from user u join application a on u.userId = a.userId join position  p on p.positionId = a.positionId join message m on a.applicationId = m.applicationId where a.hrId =#{hrId} and a.state >= #{state} and p.positionId in ",
+            "select m.news,a.state,m.msgSendTime,m.isCheck,a.applicationId,u.*,p.title from user u join application a on u.userId = a.userId join position  p on p.positionId = a.positionId join message m on a.applicationId = m.applicationId where a.hrId =#{hrId} and a.state >= #{state} and p.positionId in ",
             "<foreach collection='positionIds' item='item' index='index' open='(' separator=',' close=')'>",
             "(#{item})",
             "</foreach>",
@@ -33,7 +33,7 @@ public interface InterviewMapper {
     List<InterviewDescBO> getAllOrMoreInterviewDescByPosition(@Param("hrId")int hrId,@Param("state")int state,@Param("positionIds") List<Integer> positionIds);
 
     @Select({"<script>",
-            "select m.news,a.state,m.msgSendTime,a.applicationId,u.*,p.title from user u join application a on u.userId = a.userId join position  p on p.positionId = a.positionId join message m on a.applicationId = m.applicationId where a.hrId =#{hrId} and a.state = #{state} and p.positionId in ",
+            "select m.news,a.state,m.msgSendTime,m.isCheck,a.applicationId,u.*,p.title from user u join application a on u.userId = a.userId join position  p on p.positionId = a.positionId join message m on a.applicationId = m.applicationId where a.hrId =#{hrId} and a.state = #{state} and p.positionId in ",
             "<foreach collection='positionIds' item='item' index='index' open='(' separator=',' close=')'>" ,
             "(#{item})",
             "</foreach>",
@@ -42,7 +42,7 @@ public interface InterviewMapper {
     List<InterviewDescBO> getInterviewByStateAndPosition(@Param("hrId")int hrId,@Param("state")int state,@Param("positionIds") List<Integer> positionIds);
 
 
-    @Select("select m.news,a.state,m.msgSendTime,a.applicationId,u.*,p.title \n"+
+    @Select("select m.news,a.state,m.msgSendTime,m.isCheck,a.applicationId,u.*,p.title \n"+
             "from user u join application a on u.userId = a.userId \n" +
             "join position  p on p.positionId = a.positionId \n" +
             "join message m on a.applicationId = m.applicationId \n" +
